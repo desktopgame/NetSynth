b0c979fe09ab658ad578d0d386bf66a2e97f70aa
Koya <koya0306@outlook.jp>
2020-11-02 10:00:57 +0900

    MIDIキーボードからの入力機能仮実装

---
+++ src/jp/desktopgame/netsynth/core/MidiInputPane.java
@@ -0,0 +1,208 @@
+/*
+ * NetSynth
+ *
+ * Copyright (c) 2020 desktopgame
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+package jp.desktopgame.netsynth.core;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Transmitter;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import jp.desktopgame.netsynth.NetSynth;
+import jp.desktopgame.netsynth.core.project.ProjectSetting;
+import jp.desktopgame.netsynth.core.project.TrackSetting;
+import jp.desktopgame.netsynth.midi.MidiDeviceController;
+import jp.desktopgame.netsynth.midi.MidiDeviceManager;
+import jp.desktopgame.pec.PropertyEditorPane;
+import jp.desktopgame.sbc.SlotCallback;
+
+/**
+ *
+ * @author desktopgame
+ */
+public class MidiInputPane extends JPanel implements SlotCallback {
+
+    private List<MidiDeviceController> controllers;
+    private DefaultComboBoxModel<String> keyboardComboBoxModel;
+    private JComboBox<String> keyboardComboBox;
+
+    private DefaultComboBoxModel<String> trackComboBoxModel;
+    private JComboBox<String> trackComboBox;
+
+    private JCheckBox enabledCheckBox;
+    private Map<Transmitter, MyReceiver> trMap;
+    private static Timer swingTimer;
+
+    static {
+        swingTimer = new Timer(0, (e) -> {
+        });
+        swingTimer.setDelay(1000);
+        swingTimer.setRepeats(true);
+    }
+
+    public MidiInputPane() {
+        super(new BorderLayout());
+        this.controllers = new ArrayList<>();
+        this.keyboardComboBox = new JComboBox<>(this.keyboardComboBoxModel = new DefaultComboBoxModel<>());
+        this.trackComboBox = new JComboBox<>(this.trackComboBoxModel = new DefaultComboBoxModel<>());
+        this.enabledCheckBox = new JCheckBox();
+        this.trMap = new HashMap<>();
+        PropertyEditorPane editor = new PropertyEditorPane() {
+            {
+                addLine(new JLabel("任意のMIDIキーボードの入力をリアルタイムでトラックへ送信します。"));
+                addLine("MIDIキーボード", keyboardComboBox);
+                addLine("対象トラック", trackComboBox);
+                addLine("有効", enabledCheckBox);
+                addFooter();
+            }
+        };
+        keyboardComboBox.addItemListener(this::onSelectKeyboard);
+        trackComboBox.addItemListener(this::onSelectTrack);
+        enabledCheckBox.addActionListener(this::onEnabled);
+        add(editor, BorderLayout.CENTER);
+    }
+
+    private void onSelectKeyboard(ItemEvent e) {
+        int i = keyboardComboBox.getSelectedIndex();
+        if (i >= 0) {
+            Transmitter t = controllers.get(i).getTransmitter().get();
+            enabledCheckBox.setSelected(false);
+            if (trMap.containsKey(t)) {
+                enabledCheckBox.setSelected(trMap.get(t).enabled);
+            }
+        }
+    }
+
+    private void onSelectTrack(ItemEvent e) {
+        getReceiver().ifPresent((r) -> {
+            ProjectSetting ps = ProjectSetting.Context.getProjectSetting();
+            r.ts = ps.getTrackSetting(trackComboBox.getSelectedIndex());
+            r.enabled = enabledCheckBox.isSelected();
+            r.logInfo();
+        });
+    }
+
+    private void onEnabled(ActionEvent e) {
+        getReceiver().ifPresent((r) -> {
+            r.enabled = enabledCheckBox.isSelected();
+            r.logInfo();
+        });
+    }
+
+    private Optional<MyReceiver> getReceiver() {
+        int i = keyboardComboBox.getSelectedIndex();
+        if (i < 0) {
+            return Optional.empty();
+        }
+        int track = trackComboBox.getSelectedIndex();
+        if (track < 0) {
+            return Optional.empty();
+        }
+        ProjectSetting ps = ProjectSetting.Context.getProjectSetting();
+        Transmitter t = controllers.get(i).getTransmitter().get();
+        if (!trMap.containsKey(t)) {
+            MyReceiver mr = new MyReceiver(ps.getTrackSetting(track), t.getReceiver());
+            trMap.put(t, mr);
+            t.setReceiver(mr);
+        }
+        return Optional.of(trMap.get(t));
+    }
+
+    @Override
+    public void onShow() {
+        keyboardComboBoxModel.removeAllElements();
+        trackComboBoxModel.removeAllElements();
+        enabledCheckBox.setSelected(false);
+        this.controllers = MidiDeviceManager.getInstance().getDeviceControllers().stream().filter((e) -> e.getMaxTransmitters() > 0 || e.getMaxTransmitters() == -1).collect(Collectors.toList());
+        ProjectSetting ps = ProjectSetting.Context.getProjectSetting();
+        controllers.stream().map((e) -> e.getAlias()).forEach(keyboardComboBoxModel::addElement);
+        for (int i = 0; i < ps.getGUITrackSettingCount(); i++) {
+            trackComboBoxModel.addElement(ps.getTrackSetting(i).getName());
+        }
+    }
+
+    @Override
+    public void onHide() {
+    }
+
+    private class MyReceiver implements Receiver {
+
+        private Receiver receiver;
+        public TrackSetting ts;
+        public boolean enabled;
+
+        public MyReceiver(TrackSetting ts, Receiver receiver) {
+            this.ts = ts;
+            this.receiver = receiver;
+            this.enabled = true;
+            ProjectSetting ps = ProjectSetting.Context.getProjectSetting();
+            swingTimer.addActionListener((e) -> {
+                boolean found = false;
+                for (int i = 0; i < ps.getGUITrackSettingCount(); i++) {
+                    TrackSetting g = ps.getTrackSetting(i);
+                    if (g.equals(ts)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    enabled = false;
+                    logInfo();
+                }
+            });
+        }
+
+        public void logInfo() {
+            if (this.enabled) {
+                NetSynth.logInformation(String.format("接続されました: %s", ts.getName()));
+            } else {
+                NetSynth.logInformation(String.format("接続が解除されました: %s", ts.getName()));
+            }
+        }
+
+        @Override
+        public void send(MidiMessage arg0, long arg1) {
+            if (receiver != null) {
+                receiver.send(arg0, arg1);
+            }
+            if (!(arg0 instanceof ShortMessage)) {
+                return;
+            }
+            ShortMessage sm = (ShortMessage) arg0;
+            int height = sm.getData1();
+            int velocity = sm.getData2();
+            if (sm.getCommand() == ShortMessage.NOTE_ON) {
+                NetSynth.getView().getWorkAreaPane().noteOn(ts, height, velocity);
+            } else if (sm.getCommand() == ShortMessage.NOTE_OFF) {
+                NetSynth.getView().getWorkAreaPane().noteOff(ts, height, velocity);
+            }
+        }
+
+        @Override
+        public void close() {
+            if (receiver != null) {
+                receiver.close();
+            }
+        }
+
+    }
+}
