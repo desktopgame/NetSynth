e86f8dcb22f53b31f62c6631da6ef7dce9e17209
Koya <koya0306@outlook.jp>
2020-11-03 16:45:39 +0900

    pythonのパスを簡単に取得するためのユーティリティを追加

---
+++ src/jp/desktopgame/netsynth/core/PythonUtil.java
@@ -0,0 +1,85 @@
+/*
+ * NetSynth
+ *
+ * Copyright (c) 2020 desktopgame
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+package jp.desktopgame.netsynth.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import org.apache.commons.io.IOUtils;
+
+/**
+ *
+ * @author desktopgame
+ */
+public class PythonUtil {
+
+    private PythonUtil() {
+    }
+
+    private static Optional<String> wrap(String s) {
+        if (s == null || s.equals("")) {
+            return Optional.empty();
+        }
+        return Optional.of(s);
+    }
+
+    public static Optional<String> getSystemPythonPath() {
+        try {
+            Process proc = ProcessUtil.createShell("where python", "which python").start();
+            proc.waitFor();
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(proc.getInputStream(), writer, Charset.forName("UTF-8"));
+            return wrap(writer.toString().trim());
+        } catch (InterruptedException | IOException ex) {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<String> getPyenvPath() {
+        String homeDir = System.getenv("HOME");
+        File f = new File(String.format("/usr/local/bin/pyenv", homeDir));
+        if (f.exists()) {
+            return Optional.of(f.getPath());
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<String> getPyenvPythonPath() {
+        return getPyenvPythonPath("");
+    }
+
+    public static Optional<String> getPyenvPythonPath(String version) {
+        Optional<String> pathOpt = getPyenvPath().map((pyenv) -> {
+            try {
+                String cmd = String.format("%s shims", pyenv);
+                Process proc = ProcessUtil.createShell(cmd, cmd).start();
+                proc.waitFor();
+                StringWriter outW = new StringWriter();
+                StringWriter errW = new StringWriter();
+                IOUtils.copy(proc.getErrorStream(), errW, Charset.forName("UTF-8"));
+                String x = errW.toString();
+                IOUtils.copy(proc.getInputStream(), outW, Charset.forName("UTF-8"));
+                List<String> lines = Arrays.asList(outW.toString().split(System.lineSeparator()));
+                String name = String.format("python%s", version);
+                return lines.stream().filter((e) -> e.endsWith(name)).findFirst().map((e) -> e.trim()).orElse("");
+            } catch (InterruptedException | IOException ex) {
+                return "";
+            }
+        });
+        if (pathOpt.isPresent()) {
+            String path = pathOpt.get();
+            return wrap(path);
+        }
+        return Optional.empty();
+    }
+}
